10cb7a3f62a0c2ee1ad9825bc8634c1e2041b5b7
aya2453 <aya.nk.0116@gmail.com>
2019-02-21 00:30:09 +0900

    Fixed Gradle Kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 
 plugins {
-    id("org.jetbrains.kotlin.jvm").version("1.3.21")
+    kotlin("jvm") version "1.3.21"
 }
 
 repositories {
@@ -8,5 +8,5 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation(kotlin("stdlib"))
 }
